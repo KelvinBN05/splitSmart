d32fe9e011ef80d330d75ef82a3533cf32100802
KelvinBN05 <kelvinbn05@gmail.com>
2026-03-06 19:56:59 -0500

    Add Firebase AppCheck

--- ReceiptSplitter/ReceiptSplitterApp.swift
+++ ReceiptSplitter/ReceiptSplitterApp.swift
@@ -1,5 +1,24 @@
 import SwiftUI
 import FirebaseCore
+#if os(iOS)
+import FirebaseAppCheck
+#endif
+
+#if os(iOS)
+private final class SplitSmartAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+    func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+#if DEBUG
+        return AppCheckDebugProvider(app: app)
+#else
+        if #available(iOS 14.0, *) {
+            return AppAttestProvider(app: app)
+        } else {
+            return DeviceCheckProvider(app: app)
+        }
+#endif
+    }
+}
+#endif
 
 @main
 struct ReceiptSplitterApp: App {
@@ -7,6 +26,9 @@ struct ReceiptSplitterApp: App {
     @StateObject private var sessionStore: SessionStore
 
     init() {
+#if os(iOS)
+        AppCheck.setAppCheckProviderFactory(SplitSmartAppCheckProviderFactory())
+#endif
         FirebaseApp.configure()
         _sessionStore = StateObject(wrappedValue: SessionStore())
     }
